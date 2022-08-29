713285ceaa7f9529ab482b8c841cc2088d77499f
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-29 23:27:10 +0300

    Task finished

---
+++ Task-1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*----------------------------------------------------------------------------------------------------------------------
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+----------------------------------------------------------------------------------------------------------------------*/
+
+int MyPow(int a, int b)
+{
+    int res = a;
+    for(int i=1; i<b; i++)
+        res*=a;
+    return res;
+}
+
+Console.Write("Enter natural nuber A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Enter natural nuber B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine($"Number {a} to the power of {b} is {MyPow(a,b)}");
+
+
---
+++ Task-3/Program.cs
@@ -0,0 +1,33 @@
+﻿/*----------------------------------------------------------------------------------------------------------------------
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+----------------------------------------------------------------------------------------------------------------------*/
+
+void FillAndPrintArray(int num)
+{
+    int[] x = new int[num];
+    for(int i=0; i<num;i++)
+    {
+        Console.Write($"Please enter x[{i}]: ");
+        x[i]=Convert.ToInt32(Console.ReadLine());
+    }
+
+    Console.Write("Array you entered is: [");
+
+    for(int i=0; i<num;i++)
+    {
+        if(i==num-1)
+            Console.Write($"{x[i]}");
+        else
+            Console.Write($"{x[i]},");
+    }
+    Console.WriteLine("]");
+}
+
+Console.Write("Please enter array size: ");
+int arrSize = Convert.ToInt32(Console.ReadLine());
+FillAndPrintArray(arrSize);
+
